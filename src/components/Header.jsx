9086bf7e2e49852c2c7248629569e0367796497e
Pascal Mairot <168566181+PA-MAI@users.noreply.github.com>
2025-07-24 18:55:08 +0200

    build app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
-
-import Logo from "../assets/wealth-health-logo.svg";
+import Logo from "../assets/wealth-health-logo.svg?react";
 
 export default function Header() {
   return (
     <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
       <div className="logo">
-        <img src={Logo} className="main-logo-image" alt="HRnet Logo" />
+        <Logo className="main-logo-image" title="HRnet Logo" />
       </div>
     </Link>
   );
